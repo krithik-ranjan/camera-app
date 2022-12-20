121ec2ee288e90ab71f903ed027da4d1ff550dcf
Krithik Ranjan <krithik.ranjan@gmail.com>
2022-12-20 13:22:20 -0700

    bug

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ cameraTrigger.onClick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image, webp");
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
 }
 
